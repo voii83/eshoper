d24065e5efbd6eddf05b9b20b5a5f88aec7ebf69
voii83 <voiivoii83@gmail.com>
2018-12-19 12:09:32 +0300

    Вывод категорий товаров

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -14,4 +14,11 @@ class CategoryController extends AppController
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         return $this->render('index', compact('hits'));
     }
+
+    public function actionView($id)
+    {
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])->all();
+        return $this->render('view', compact('products'));
+    }
 }
---
+++ views/category/view.php
@@ -0,0 +1,107 @@
+<?php
+use yii\helpers\Html;
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+
+<section id="advertisement">
+    <div class="container">
+        <?= Html::img('@web/images/shop/ad.jpg', ['alt' => '']) ?>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="left-sidebar">
+                    <h2>Category</h2>
+                    <ul class="menu-accordion panel-group category-products">
+                        <?= \app\components\menuWidget\MenuWidget::widget(['tpl' => 'menu']); ?>
+                    </ul><!--/category-products-->
+                    <!--/category-productsr-->
+
+                    <div class="brands_products"><!--brands_products-->
+                        <h2>Brands</h2>
+                        <div class="brands-name">
+                            <ul class="nav nav-pills nav-stacked">
+                                <li><a href=""> <span class="pull-right">(50)</span>Acne</a></li>
+                                <li><a href=""> <span class="pull-right">(56)</span>Grüne Erde</a></li>
+                                <li><a href=""> <span class="pull-right">(27)</span>Albiro</a></li>
+                                <li><a href=""> <span class="pull-right">(32)</span>Ronhill</a></li>
+                                <li><a href=""> <span class="pull-right">(5)</span>Oddmolly</a></li>
+                                <li><a href=""> <span class="pull-right">(9)</span>Boudestijn</a></li>
+                                <li><a href=""> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
+                            </ul>
+                        </div>
+                    </div><!--/brands_products-->
+
+                    <div class="price-range"><!--price-range-->
+                        <h2>Price Range</h2>
+                        <div class="well">
+                            <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
+                            <b>$ 0</b> <b class="pull-right">$ 600</b>
+                        </div>
+                    </div><!--/price-range-->
+
+                    <div class="shipping text-center"><!--shipping-->
+                        <?= Html::img('@web/images/home/shipping.jpg', ['alt' => '']) ?>
+                    </div><!--/shipping-->
+
+                </div>
+            </div>
+
+            <div class="col-sm-9 padding-right">
+                <div class="features_items"><!--features_items-->
+                    <h2 class="title text-center">Features Items</h2>
+                    <?php if (!empty($products)) : ?>
+                        <?php foreach ($products as $product) : ?>
+                            <div class="col-sm-4">
+                                <div class="product-image-wrapper">
+                                    <div class="single-products">
+                                        <div class="productinfo text-center">
+                                            <?= Html::img("@web/images/home/{$product->img}", ['alt' => '']) ?>
+                                            <h2>$<?= $product->price ?></h2>
+                                            <p><?= $product->name ?></p>
+                                            <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                        </div>
+                                        <div class="product-overlay">
+                                            <div class="overlay-content">
+                                                <h2>$<?= $product->price ?></h2>
+                                                <p><?= $product->name ?></p>
+                                                <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                            </div>
+                                        </div>
+                                        <?php if ($product->new) : ?>
+                                            <?= Html::img("@web/images/home/new.png", ['alt' => 'Новинка', 'class' => 'new']) ?>
+                                        <?php endif; ?>
+                                        <?php if ($product->sale) : ?>
+                                            <?= Html::img("@web/images/home/sale.png", ['alt' => 'Распродажа', 'class' => 'sale']) ?>
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="choose">
+                                        <ul class="nav nav-pills nav-justified">
+                                            <li><a href=""><i class="fa fa-plus-square"></i>Add to wishlist</a></li>
+                                            <li><a href=""><i class="fa fa-plus-square"></i>Add to compare</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                     <?php else : ?>
+                        <h2>Товары не найдены</h2>
+                    <?php endif; ?>
+                    <div class="clearfix"></div>
+                    <ul class="pagination">
+                        <li class="active"><a href="">1</a></li>
+                        <li><a href="">2</a></li>
+                        <li><a href="">3</a></li>
+                        <li><a href="">&raquo;</a></li>
+                    </ul>
+
+                </div><!--features_items-->
+            </div>
+        </div>
+    </div>
+</section>
