4d9ae1a07f559350c53b6eeb0c16d3fd111502a3
voii83 <voiivoii83@gmail.com>
2018-12-30 12:26:14 +0300

    Оформление корзины

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -7,8 +7,11 @@
  */
 
 namespace app\controllers;
+use app\components\Debug;
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItems;
 use Yii;
 
 /*Array
@@ -92,6 +95,41 @@ class CartController extends AppController{
         $session = Yii::$app->session;
         $session->open();
 
-        return $this->render('view', compact('session'));
+        $this->setMeta('Корзина');
+
+        $order = new Order();
+        if ($order->load(Yii::$app->request->post())) {
+            $order->qty = $session['cart.qty'];
+            $order->sum = $session['cart.sum'];
+
+            if ($order->save()) {
+                $this->saveOrderItems($session['cart'], $order->id);
+                Yii::$app->session->setFlash('success', 'Ваш заказ принят. Менеджер вскоре свяжется с вами.');
+
+                $session->remove('cart');
+                $session->remove('cart.qty');
+                $session->remove('cart.sum');
+
+                return $this->refresh();
+            }
+            else {
+                Yii::$app->session->setFlash('error', 'Ошибка оформления заказа.');
+            }
+        }
+        return $this->render('view', compact('session', 'order'));
+    }
+
+    protected function saveOrderItems($items, $order_id)
+    {
+        foreach ($items as $id => $item) {
+            $order_items = new OrderItems();
+            $order_items->order_id = $order_id;
+            $order_items->product_id = $id;
+            $order_items->name = $item['name'];
+            $order_items->price = $item['price'];
+            $order_items->qty_item = $item['qty'];
+            $order_items->sum_item = $item['qty'] * $item['price'];
+            $order_items->save();
+        }
     }
 }
